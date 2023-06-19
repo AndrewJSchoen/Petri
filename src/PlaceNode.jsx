eaff5d880839c433327a2f993fa1f870bc7231a3
Andrew Schoen <schoen.andrewj@gmail.com>
2023-06-19 17:56:42 -0500

    Update PlaceNode.jsx
    You can now change the type of place node

--- src/PlaceNode.jsx
+++ src/PlaceNode.jsx
@@ -1,4 +1,11 @@
-import { Avatar, InputAdornment, TextField } from "@mui/material";
+import {
+  Avatar,
+  InputAdornment,
+  TextField,
+  ToggleButtonGroup,
+  ToggleButton,
+  Stack,
+} from "@mui/material";
 import React, { memo, useMemo, useState } from "react";
 import { Handle, Position, NodeToolbar, useNodeId, useStore } from "reactflow";
 import { useAtom } from "jotai";
@@ -7,7 +14,7 @@ import { placesAtom, selectedNodeAtom, transitionsAtom } from "./atom";
 import { BsPinAngle, BsPinAngleFill } from "react-icons/bs";
 import { IoInfinite, IoExitOutline } from "react-icons/io5";
 import { FiMinus, FiPlus, FiTrash2 } from "react-icons/fi";
-import { pickBy, mapValues } from "lodash";
+import { mapValues } from "lodash";
 
 const connectionNodeIdSelector = (state) => state.connectionNodeId;
 
@@ -35,82 +42,111 @@ export default memo(({ isConnectable }) => {
           className="nodrag nopan"
           isVisible={selectedNode === nodeId || pinned}
         >
-          <TextField
-            autoFocus
-            size="small"
-            value={place.name}
-            label="Place Name"
-            variant="outlined"
-            onChange={(e) => {
-              setPlace({ ...place, name: e.target.value });
-            }}
-            color="info"
-            InputProps={{
-              style: {
-                color: "#ddd",
-                backgroundColor: "#77777777",
-              },
-              startAdornment: (
-                <InputAdornment
-                  position="start"
-                  onClick={() => setPinned(!pinned)}
-                >
-                  {pinned ? <BsPinAngleFill /> : <BsPinAngle />}
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <>
-                  {place.tokens !== "infinite" && place.tokens !== "sink" && (
-                    <InputAdornment position="end">
-                      <FiMinus
-                        onClick={() => {
-                          setPlace({
-                            ...place,
-                            tokens: place.tokens.slice(0, -1),
-                          });
-                        }}
-                      />
-                    </InputAdornment>
-                  )}
-                  {place.tokens !== "infinite" && place.tokens !== "sink" && (
-                    <InputAdornment position="end">
-                      <FiPlus
-                        onClick={() => {
-                          setPlace({ ...place, tokens: [...place.tokens, {}] });
-                        }}
-                      />
-                    </InputAdornment>
-                  )}
+          <Stack direction="column" alignItems="center" spacing={2}>
+            <ToggleButtonGroup
+              exclusive
+              color='primary'
+              style={{ height: 20 }}
+              value={typeof place.tokens === "string" ? place.tokens : "#"}
+              onChange={(_,newValue) => {
+                if (newValue === "#") {
+                  setPlace({ ...place, tokens: [] });
+                } else {
+                  setPlace({ ...place, tokens: newValue });
+                }
+                
+              }}
+            >
+              <ToggleButton value="infinite">
+                <IoInfinite />
+              </ToggleButton>
+              <ToggleButton value="#">#</ToggleButton>
+              <ToggleButton value="sink">
+                <IoExitOutline />
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <TextField
+              autoFocus
+              size="small"
+              value={place.name}
+              label="Place Name"
+              variant="outlined"
+              onChange={(e) => {
+                setPlace({ ...place, name: e.target.value });
+              }}
+              color="info"
+              InputProps={{
+                style: {
+                  color: "#ddd",
+                  backgroundColor: "#77777777",
+                },
+                startAdornment: (
                   <InputAdornment
-                    position="end"
-                    onClick={() => {
-                      const { [place.id]: _, ...rest } = places;
-                      console.log("rest", rest);
-                      const newTransitions = mapValues(
-                        transitions,
-                        (transition) => {
-                          const { [place.id]: _i, ...input } = transition.input;
-                          const { [place.id]: _o, ...output } = transition.output;
-                          return { ...transition, input, output };
-                        }
-                      );
-                      console.log({id:place.id,rest,newTransitions});
-                      setPlaces(rest);
-                      setTransitions(newTransitions);
-                    }}
+                    position="start"
+                    onClick={() => setPinned(!pinned)}
                   >
-                    <FiTrash2 />
+                    {pinned ? <BsPinAngleFill /> : <BsPinAngle />}
                   </InputAdornment>
-                </>
-              ),
-            }}
-            inputProps={{
-              style: {
-                maxWidth: 100,
-                textAlign: "center",
-              },
-            }}
-          />
+                ),
+                endAdornment: (
+                  <>
+                    {place.tokens !== "infinite" && place.tokens !== "sink" && (
+                      <InputAdornment position="end">
+                        <FiMinus
+                          onClick={() => {
+                            setPlace({
+                              ...place,
+                              tokens: place.tokens.slice(0, -1),
+                            });
+                          }}
+                        />
+                      </InputAdornment>
+                    )}
+                    {place.tokens !== "infinite" && place.tokens !== "sink" && (
+                      <InputAdornment position="end">
+                        <FiPlus
+                          onClick={() => {
+                            setPlace({
+                              ...place,
+                              tokens: [...place.tokens, {}],
+                            });
+                          }}
+                        />
+                      </InputAdornment>
+                    )}
+                    <InputAdornment
+                      position="end"
+                      onClick={() => {
+                        const { [place.id]: _, ...rest } = places;
+                        console.log("rest", rest);
+                        const newTransitions = mapValues(
+                          transitions,
+                          (transition) => {
+                            const { [place.id]: _i, ...input } =
+                              transition.input;
+                            const { [place.id]: _o, ...output } =
+                              transition.output;
+                            return { ...transition, input, output };
+                          }
+                        );
+                        console.log({ id: place.id, rest, newTransitions });
+                        setPlaces(rest);
+                        setTransitions(newTransitions);
+                      }}
+                    >
+                      <FiTrash2 />
+                    </InputAdornment>
+                  </>
+                ),
+              }}
+              inputProps={{
+                style: {
+                  maxWidth: 100,
+                  textAlign: "center",
+                },
+              }}
+            />
+          </Stack>
         </NodeToolbar>
 
         <Handle
@@ -145,7 +181,7 @@ export default memo(({ isConnectable }) => {
           ) : place.tokens === "sink" ? (
             <IoExitOutline />
           ) : (
-            place.tokens.length > 0 && place.tokens.length
+            place?.tokens?.length > 0 && place.tokens.length
           )}
         </Avatar>
 
